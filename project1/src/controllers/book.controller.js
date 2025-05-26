619432699b06bb008ddef1c6cafe836b5b9c4748
NOUR904 <20912023200877@fci.zu.edu.eg>
2025-05-26 05:41:12 +0300

    Update book.controller.js

--- project1/src/controllers/book.controller.js
+++ project1/src/controllers/book.controller.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 import { Book } from "../models/book.model.js";
 import { NotFoundError, BadRequestError } from "../errors/Errors.js";
 
-<<<<<<< HEAD
-=======
 
->>>>>>> c5f25293fa6d9b02ec3072c7936fe855ac2a89af
+
+
 export const addNewBookController = async (req, res) => {
   try {
     const book = req.body;
@@ -84,8 +83,7 @@ export const getAllBooksController = async (req, res) => {
     res.status(400).send(err.message);
   }
 };
-<<<<<<< HEAD
-=======
+
 
     }
     catch(err){
@@ -95,4 +93,4 @@ export const getAllBooksController = async (req, res) => {
 
 }
 
->>>>>>> c5f25293fa6d9b02ec3072c7936fe855ac2a89af
+
